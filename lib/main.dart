89a2b0174383e7f1d8536476f6ded251067c2d0b
Shivakumar S <sudoshivu11@gmail.com>
2022-05-23 19:21:13 +0530

    added tabs i.e., CHATS STATUS CALLS

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
-        backgroundColor: backgroundColor,
+        scaffoldBackgroundColor: backgroundColor,
       ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
 
+import '../colors.dart';
+
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('this is mobile'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          elevation: 0,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search, color: Colors.grey),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert, color: Colors.grey),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(text: 'CHATS'),
+              Tab(text: 'STATUS'),
+              Tab(text: 'CALLS'),
+            ],
+          ),
+        ),
+        body: Text('mobile'),
       ),
     );
   }
