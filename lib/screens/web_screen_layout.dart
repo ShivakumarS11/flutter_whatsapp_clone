5d2b81f2bc59ce6db1d8475d0cb4a1b575011cd3
Shivakumar S <sudoshivu11@gmail.com>
2022-05-24 22:00:05 +0530

    added web basic webscreen layout

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('this is web'),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  //web profile bar
+                  //web search bar
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/backgroundImage.png'),
+              fit: BoxFit.cover,
+            )),
+          )
+        ],
       ),
     );
   }
